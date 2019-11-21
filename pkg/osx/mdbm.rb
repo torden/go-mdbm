4206cf63d5d15ff30ee073ff32dacbbf0b3d4def
torden <tordengit@gmail.com>
2019-11-21 14:11:19 +0900

    update mdbm.rd for homebrew

--- pkg/osx/mdbm.rb
+++ pkg/osx/mdbm.rb
@@ -6,23 +6,33 @@ class Mdbm < Formula
 
   depends_on "coreutils" => :build
   depends_on "cppunit" => :build
-  depends_on "make" => :build
-  depends_on "openssl" => :build
-  depends_on "readline" => :build
+  depends_on :xcode => ["10.2", :build]
+  depends_on "openssl@1.1"
+  depends_on "readline"
 
   def install
-    ENV.delete "CC"
-    ENV.delete "CXX"
-    system "make", "-j8"
+    ENV["DYLD_LIBRARY_PATH"] = libexec.to_s
+    inreplace "src/lib/Makefile" do |s|
+      s.gsub! "LIB_DEST=\$(PREFIX)/lib\$(ARCH_SUFFIX)", "LIB_DEST=\$(PREFIX)/libexec"
+    end
+    system "make"
+    Dir.glob("src/tools/object/*") do |f|
+      next if ["mdbm_big_data_builder.pl", "mdbm_environment.sh", "mdbm_reset_all_locks"].include? File.basename(f)
+
+      macho = MachO.open(f)
+      macho.change_dylib("object/libmdbm.dylib", "#{libexec}/libmdbm.dylib")
+      macho.write!
+    end
+
     system "make", "install", "PREFIX=#{prefix}"
   end
 
   test do
-    ts_mdbm = "/tmp/test.mdbm"
+    ts_mdbm = testpath/"test.mdbm"
     system "mdbm_create", ts_mdbm
     assert_predicate ts_mdbm, :exist?
     system "mdbm_check", ts_mdbm
-    system "mdbm_trunc", "-y", ts_mdbm
+    system "mdbm_trunc", "-f", ts_mdbm
     system "mdbm_sync", ts_mdbm
   end
 end
