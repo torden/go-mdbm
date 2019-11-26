183c77dc10540a3c89d3d30f343e170994210d8b
torden <tordengit@gmail.com>
2019-11-27 06:58:06 +0900

    update README.md, mdbm.rb

--- pkg/osx/mdbm.rb
+++ pkg/osx/mdbm.rb
@@ -4,7 +4,6 @@ class Mdbm < Formula
   url "https://github.com/yahoo/mdbm/archive/v4.13.0.tar.gz"
   sha256 "99cec32e02639048f96abf4475eb3f97fc669541560cd030992bab155f0cb7f8"
 
-  depends_on "coreutils" => :build
   depends_on "cppunit" => :build
   depends_on :xcode => ["10.2", :build]
   depends_on "openssl@1.1"
@@ -12,9 +11,30 @@ class Mdbm < Formula
 
   def install
     ENV["DYLD_LIBRARY_PATH"] = libexec.to_s
+    inreplace "Makefile.base" do |s|
+      s.gsub! "INSTALL=ginstall", "INSTALL=install"
+    end
+    inreplace "gendoc/Makefile" do |s|
+      s.gsub! "\$(INSTALL) -t \$(MAN_PREFIX)/man1 \$(shell find -L ./man/man1 -type f)", "\$(INSTALL) \$(shell find -L ./man/man1 -type f) \$(MAN_PREFIX)/man1"
+    end
+    inreplace "include/Makefile" do |s|
+      s.gsub! "\$(INSTALL) -t \$(INC_PREFIX) \$(HEADERS)", "\$(INSTALL) \$(HEADERS) \$(INC_PREFIX)"
+    end
+    inreplace "src/tools/Makefile" do |s|
+      s.gsub! "\$(INSTALL) -t \$(EXE_DEST) \$(EXES) \$(CPPEXES) \$(OBJDIR)/mdbm_bench \$(OBJDIR)/mash", \
+              "\$(INSTALL) \$(EXES) \$(CPPEXES) \$(OBJDIR)/mdbm_bench \$(OBJDIR)/mash \$(EXE_DEST)"
+    end
+    inreplace "src/scripts/Makefile" do |s|
+      s.gsub! "\$(INSTALL) -t \$(EXE_DEST) \$(SCRIPTS)", "\$(INSTALL) \$(SCRIPTS) \$(EXE_DEST)"
+    end
+    inreplace "src/java/Makefile" do |s|
+      s.gsub! "\$(INSTALL) -D \$(OBJDIR)/\$(LIBNAME).\$(LIBVER) \$(LIB_DEST)", "\$(INSTALL) \$(OBJDIR)/\$(LIBNAME).\$(LIBVER) \$(LIB_DEST)"
+    end
     inreplace "src/lib/Makefile" do |s|
       s.gsub! "LIB_DEST=\$(PREFIX)/lib\$(ARCH_SUFFIX)", "LIB_DEST=\$(PREFIX)/libexec"
+      s.gsub! "\$(INSTALL) -D \$(OBJDIR)/\$(LIBNAME).\$(LIBVER) \$(LIB_DEST)", "\$(INSTALL) \$(OBJDIR)/\$(LIBNAME)\.\$(LIBVER) \$(LIB_DEST)"
     end
+
     system "make"
     Dir.glob("src/tools/object/*") do |f|
       next if ["mdbm_big_data_builder.pl", "mdbm_environment.sh", "mdbm_reset_all_locks"].include? File.basename(f)
